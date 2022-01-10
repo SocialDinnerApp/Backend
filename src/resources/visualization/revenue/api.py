26a60821d002bef3b03b5fffd2b2d4ab56002800
ilyas <ilyas.boeh@curemannheim.de>
2022-01-10 21:38:08 +0100

    update: MonthlyRevenueAPI

--- src/resources/visualization/revenue/api.py
+++ src/resources/visualization/revenue/api.py
@@ -1,4 +1,4 @@
-from math import e
+from math import e, nan
 from src.resources.event.model import Event
 from src.resources.event_participations.model import EventParticipation
 from src.resources.visualization.revenue.args import post_args
@@ -10,7 +10,7 @@ import datetime
 from src import db
 
 
-class MontlyRevenueAPI(Resource):
+class MonthlyRevenueAPI(Resource):
 
     # @jwt_required()
     #@marshal_with(resource_fields)
@@ -23,24 +23,42 @@ class MontlyRevenueAPI(Resource):
         #organizer_id = get_jwt_identity()
 
         event_this_month = db.session.query(Event).filter((datetime.datetime.now().month == extract('month', Event.registration_deadline)) and (Event.org_Id == args['organizerId'])).all()
-        
+        events = db.session.query(Event).filter(Event.org_Id == args['organizerId']).all()
+
         count_revenu = 0
-        for event in event_this_month:
+        for event in events:
+            print(f'Event:', event)
             sum_of_participants = 0
-            eventId = event.eventId
-            participants = db.session.query(EventParticipation).filter(EventParticipation.eventId == eventId).all()
+            participants = db.session.query(EventParticipation).filter(EventParticipation.eventId == event.eventId, extract('month', EventParticipation.datetime_created) == datetime.datetime.now().month).all()
+            print(f'liste',participants)
             for participant in participants:
                 sum_of_participants += 1
             event_revenue = 0
-            event_revenue = event_revenue + sum_of_participants * event.fee
+            event_revenue = sum_of_participants * event.fee
             count_revenu = count_revenu + sum_of_participants * event.fee
             print(f'Sum of participants: for {event}:', sum_of_participants)
             print(f'Revenue:', event_revenue)
 
         print(f'Overall revenue:', count_revenu)
+        
+
+        # count_revenu = 0
+        # for event in event_this_month:
+        #     sum_of_participants = 0
+        #     eventId = event.eventId
+        #     participants = db.session.query(EventParticipation).filter(EventParticipation.eventId == eventId).all()
+        #     for participant in participants:
+        #         sum_of_participants += 1
+        #     event_revenue = 0
+        #     event_revenue = event_revenue + sum_of_participants * event.fee
+        #     count_revenu = count_revenu + sum_of_participants * event.fee
+        #     print(f'Sum of participants: for {event}:', sum_of_participants)
+        #     print(f'Revenue:', event_revenue)
+
+        # print(f'Overall revenue:', count_revenu)
 
 
-        return jsonify(revenue=count_revenu)
+        # return jsonify(revenue=count_revenu)
 
 
 
